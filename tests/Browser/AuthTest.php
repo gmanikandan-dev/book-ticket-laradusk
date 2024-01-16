5c73f86e134aa26023f79df713330355de22c13c
gmanikandan-dev <gmanikandan845@gmail.com>
2024-01-16 10:31:17 +0530

    code standard format

--- tests/Browser/AuthTest.php
+++ tests/Browser/AuthTest.php
@@ -3,13 +3,13 @@
 namespace Tests\Browser;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Log;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class AuthTest extends DuskTestCase
 {
     use DatabaseMigrations;
+
     /**
      * A Dusk test register.
      */
@@ -18,21 +18,21 @@ class AuthTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             //We'll test the register feature here
             $browser->visit('/')
-                    ->clickLink('Register')
-                    ->value('#name', 'Admin') 
-                    ->value('#email', 'admin@test.com')
-                    ->value('#password', 'password')
-                    ->value('#password_confirmation', 'password')
-                    ->click('button[type="submit"]')
-                    ->assertSee('Welcome to your Jetstream application')
-                    ->press('Admin')
-                    ->click('@logout')
-                    ->assertSee('News')
-                    ->clickLink('Log in')
-                    ->value('#email', 'admin@test.com')
-                    ->value('#password', 'password')
-                    ->click('button[type="submit"]')
-                    ->assertSee('Welcome to your Jetstream application');
+                ->clickLink('Register')
+                ->value('#name', 'Admin')
+                ->value('#email', 'admin@test.com')
+                ->value('#password', 'password')
+                ->value('#password_confirmation', 'password')
+                ->click('button[type="submit"]')
+                ->assertSee('Welcome to your Jetstream application')
+                ->press('Admin')
+                ->click('@logout')
+                ->assertSee('News')
+                ->clickLink('Log in')
+                ->value('#email', 'admin@test.com')
+                ->value('#password', 'password')
+                ->click('button[type="submit"]')
+                ->assertSee('Welcome to your Jetstream application');
         });
     }
 }
