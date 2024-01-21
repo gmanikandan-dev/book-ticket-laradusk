03d57543f75a3a0e0529e013f98c89c970681540
gmanikandan-dev <gmanikandan845@gmail.com>
2024-01-21 18:59:29 +0530

    changed text

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                ->assertSee('News');
+                ->assertSee('Laravel');
         });
     }
 }
